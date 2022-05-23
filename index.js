2e098ed3ff868516e1abf70b65bb8467b9481fc9
TanzimTayef <tayef464@gmail.com>
2022-05-23 22:14:47 +0600

    added order route

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const PORT = process.env.PORT || 5000;
 
 
@@ -19,6 +19,7 @@ async function run() {
     try {
         await client.connect();
         const toolsCollection = client.db("power-tools").collection("tools");
+        const orderCollection = client.db("power-tools").collection("order");
 
         app.get("/tools", async (req, res) => {
             const query = {};
@@ -26,6 +27,19 @@ async function run() {
             const tools = await cursor.toArray();
             res.send(tools);
         });
+
+        app.get("/tools/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const tool = await toolsCollection.findOne(query);
+            res.send(tool);
+          });
+
+          app.post("/order", async (req, res) => {
+            const order = req.body;
+            const result = await orderCollection.insertOne(order);
+            res.send(result);
+        })
         
       
     
