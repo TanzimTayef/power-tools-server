8a9ed07f7d878250365464015860adfb5bf6dfad
TanzimTayef <tayef464@gmail.com>
2022-05-24 14:47:34 +0600

    adde put API for upadate user informantion

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const PORT = process.env.PORT || 5000;
-
 
 const app = express();
 
@@ -11,83 +10,97 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xeeis.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 async function run() {
-    try {
-        await client.connect();
-        const toolsCollection = client.db("power-tools").collection("tools");
-        const orderCollection = client.db("power-tools").collection("order");
-        const reviewCollection = client.db("power-tools").collection("review");
+  try {
+    await client.connect();
+    const toolsCollection = client.db("power-tools").collection("tools");
+    const orderCollection = client.db("power-tools").collection("order");
+    const reviewCollection = client.db("power-tools").collection("review");
+    const infoCollection = client.db("power-tools").collection("userInfo");
+   
 
-        app.get("/tools", async (req, res) => {
-            const query = {};
-            const cursor = toolsCollection.find(query);
-            const tools = await cursor.toArray();
-            res.send(tools);
-        });
+    app.get("/tools", async (req, res) => {
+      const query = {};
+      const cursor = toolsCollection.find(query);
+      const tools = await cursor.toArray();
+      res.send(tools);
+    });
 
+    app.get("/tools/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const tool = await toolsCollection.findOne(query);
+      res.send(tool);
+    });
 
-        app.get("/tools/:id", async (req, res) => {
-            const id = req.params.id;
-            const query = {_id: ObjectId(id)};
-            const tool = await toolsCollection.findOne(query);
-            res.send(tool);
-          });
+    app.post("/order", async (req, res) => {
+      const order = req.body;
+      const result = await orderCollection.insertOne(order);
+      res.send(result);
+    });
 
-          app.post("/order", async (req, res) => {
-            const order = req.body;
-            const result = await orderCollection.insertOne(order);
-            res.send(result);
-          })
-        
-          app.get("/order", async (req, res) => {
-            const email = req.query.email;
-            const query = {email: email};
-            const cursor = orderCollection.find(query);
-            const orders = await cursor.toArray();
-            res.send(orders);  
-          })
-        
-        app.delete("/order/:id", async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: ObjectId(id) };
-            const result = await orderCollection.deleteOne(query);
-            res.send(result);
-        });
+    app.get("/order", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const cursor = orderCollection.find(query);
+      const orders = await cursor.toArray();
+      res.send(orders);
+    });
 
-        app.get("/review", async (req, res) => {
-            const query = {};
-            const cursor = reviewCollection.find(query);
-            const review = await cursor.toArray();
-            res.send(review);
-        });
+    app.delete("/order/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await orderCollection.deleteOne(query);
+      res.send(result);
+    });
 
-        
-          app.post("/review", async (req, res) => {
-            const newTools = req.body;
-            const result = await reviewCollection.insertOne(newTools);
-            res.send(result);
-        });
-        
-      
-    
-    } finally {
-        
-}
+    app.get("/review", async (req, res) => {
+      const query = {};
+      const cursor = reviewCollection.find(query);
+      const review = await cursor.toArray();
+      res.send(review);
+    });
+
+    app.post("/review", async (req, res) => {
+      const newTools = req.body;
+      const result = await reviewCollection.insertOne(newTools);
+      res.send(result);
+    });
+
+    app.put("/user/:email", async (req, res) => {
+      const email = req.params.email;
+      const info = req.body;
+      const filter = { email: email };
+     
+      console.log(info);
+     
+      console.log(filter);
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: info,
+      };
+      const result = await infoCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      res.send(result);
+    });
+  } finally {
+  }
 }
 run().catch(console.dir);
 
-
-
-
-
 app.get("/", (req, res) => {
-    res.send("Power Tools server is running!");
-})
+  res.send("Power Tools server is running!");
+});
 
 app.listen(PORT, () => {
-   console.log('Server is running:', PORT)
-})
+  console.log("Server is running:", PORT);
+});
