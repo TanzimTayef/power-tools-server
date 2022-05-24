daf86d597b0e08170d969e2327aadb3b6b2d6cd6
TanzimTayef <tayef464@gmail.com>
2022-05-25 01:05:28 +0600

    added payment api

--- index.js
+++ index.js
@@ -174,6 +174,15 @@ async function run() {
       res.send({ admin: isAdmin });
     });
 
+
+    // payment api
+    app.get("/orders/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) }
+      const order = await orderCollection.findOne(query);
+      res.send(order);
+    })
+
   } finally {
   }
 }
