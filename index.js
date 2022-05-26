7f7daf288067528c1e4f4f2f5686f1c127a05a29
TanzimTayef <tayef464@gmail.com>
2022-05-26 12:04:00 +0600

    could not sovlved this error

--- index.js
+++ index.js
@@ -95,7 +95,7 @@ async function run() {
     app.get("/review", async (req, res) => {
       const query = {};
       const cursor = reviewCollection.find(query);
-      const review = await cursor.toArray().reverse();
+      const review = await cursor.toArray();
       res.send(review);
     });
 
