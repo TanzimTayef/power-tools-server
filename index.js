518d6caf4e19e7f0f7a4b758f439ef823a8b2d18
TanzimTayef <tayef464@gmail.com>
2022-05-26 15:58:04 +0600

    make api for save payment id corfim payment

--- index.js
+++ index.js
@@ -44,6 +44,7 @@ async function run() {
     const reviewCollection = client.db("power-tools").collection("review");
     const infoCollection = client.db("power-tools").collection("userInfo");
     const usersCollection = client.db("power-tools").collection("users");
+    const paymentCollection = client.db("power-tools").collection("payments");
 
     app.get("/tools", async (req, res) => {
       const query = {};
@@ -202,6 +203,21 @@ async function run() {
         payment_method_types:['card']
       });
       res.send({clientSecret: paymentIntent.client_secret})
+    });
+
+    app.patch("/order/:id", async (req, res) => {
+      const id = req.params.id;
+      const payment = req.body;
+      const filter = { _id: ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          paid: true,
+          transactionId: payment.transactionId
+        }
+      }
+      const result = await paymentCollection.insertOne(payment);
+      const updatedOrder = await orderCollection.updateOne(filter, updatedDoc);
+      res.send(updatedOrder);
     });
 
 
